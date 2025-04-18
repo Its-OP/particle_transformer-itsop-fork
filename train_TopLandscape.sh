a22cf5404f9d1c68c84a31e48f880b426f334798
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2025-04-18 16:05:36 +0300

    make num-epochs configurable

--- train_TopLandscape.sh
+++ train_TopLandscape.sh
@@ -62,7 +62,7 @@ weaver \
     --batch-size ${batchsize} \
     --samples-per-epoch $((2400 * 512 * 512 / ${batchsize})) \
     --samples-per-epoch-val $((800 * 512 * 512 / ${batchsize})) \
-    --num-epochs 20 \
+    --num-epochs ${num-epochs} \
     --gpus ${gpus} \
     --start-lr $lr \
     --optimizer $optimizer \
